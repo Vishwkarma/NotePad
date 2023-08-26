bfe532820274405f79bcede824f394579502c928
IAmChetan <100069295+Vishwkarma@users.noreply.github.com>
2023-08-26 16:48:30 +0530

    Add files via upload

---
+++ Notepad/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        RichTextBox1.Clear()
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveFileDialog1.ShowDialog()
+        RichTextBox1.SaveFile(SaveFileDialog1.FileName)
+
+        MsgBox("File save successfully ")
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        OpenFileDialog1.ShowDialog()
+        RichTextBox1.LoadFile(OpenFileDialog1.FileName)
+
+    End Sub
+
+    Private Sub XitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles XitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        RichTextBox1.Cut()
+
+
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        RichTextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        RichTextBox1.Paste()
+    End Sub
+
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        RichTextBox1.Redo()
+    End Sub
+
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        FontDialog1.ShowDialog()
+        RichTextBox1.SelectionFont = FontDialog1.Font
+
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
+        ColorDialog1.ShowDialog()
+        RichTextBox1.SelectionColor = ColorDialog1.Color
+
+
+    End Sub
+
+    Private Sub AlignToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AlignToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Left
+
+    End Sub
+
+    Private Sub RightToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RightToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Right
+    End Sub
+
+    Private Sub CenterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CenterToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+
+    Private Sub YesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YesToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = True
+
+    End Sub
+
+    Private Sub NoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NoToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = False
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        RichTextBox1.ZoomFactor = 2
+
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+        RichTextBox1.ZoomFactor = 4
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        RichTextBox1.ZoomFactor = 6
+    End Sub
+
+    Private Sub ToolStripMenuItem5_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem5.Click
+        RichTextBox1.ZoomFactor = 8
+    End Sub
+
+    Private Sub ToolStripMenuItem6_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem6.Click
+        RichTextBox1.ZoomFactor = 10
+    End Sub
+End Class
